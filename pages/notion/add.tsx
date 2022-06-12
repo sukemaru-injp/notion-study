71571044b24be34ae64ff9a12dc0414510361135
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-06-12 12:15:54 +0900

    first-commit

---
+++ pages/notion/add.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const NotionAddPage: NextPage = () => {
+  return (
+    <></>
+  )
+}
+
+export default NotionAddPage
---
+++ pages/notion/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const NotionPage: NextPage = () => {
+  return (
+    <></>
+  )
+}
+
+export default NotionPage
