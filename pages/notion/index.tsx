e27a68ec457ee598377092ac12ec85b7f1f81a71
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-06-13 13:49:01 +0900

    中身のみ取得

--- pages/notion/index.tsx
+++ pages/notion/index.tsx
@@ -3,11 +3,10 @@ import { NextPage, GetServerSideProps } from 'next'
 import notion from '../../libs/notion'
 interface PageProps {
   res: any
-  page: any
 }
 
-const NotionPage: NextPage<PageProps> = ({ res, page }: PageProps) => {
-  console.log('fetch', res, page)
+const NotionPage: NextPage<PageProps> = ({ res }: PageProps) => {
+  console.log('fetch', res)
   return (
     <>
       <h2>Notion Index</h2>
@@ -19,11 +18,9 @@ export default NotionPage
 
 export const getServerSideProps: GetServerSideProps = async () => {
   // Fetch data from external API
-  const res = await notion.databases.retrieve({ database_id: process?.env?.NOTION_DATABASE_ID || '' })
-
-  const page = await notion.pages.retrieve({ page_id: process?.env?.NOTION_PAGE_ID || '' })
+  const res = await notion.databases.query({ database_id: process?.env?.NOTION_DATABASE_ID || '' })
 
   // Pass data to the page via props
-  return { props: { res, page } }
+  return { props: { res } }
 }
 
